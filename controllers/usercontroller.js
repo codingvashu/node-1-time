ea274c24c85443fd8621aadfbc9aa8f0e8477b29
codingvashu <tyagideepanshu068@gmail.com>
2024-09-14 07:30:55 +0530

    upload

---
+++ controllers/usercontroller.js
@@ -0,0 +1,12 @@
+const users = require('../users.json');
+function getAllUser(req,res){
+    try{
+        res.json(users)
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {
+    getAllUser
+}
